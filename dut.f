0f29f71b8af020ef990c854e129c86bcd72f193f
SteveYaoLin <yaolin0616@163.com>
2024-06-30 19:59:44 +0800

    出自FPGA奇哥。https://www.bilibili.com/video/BV1tv4y1y7VX/?spm_id_from=333.999.0.0 本地源D:\study\lhz7020prj\QIGEFPGA\TEST-06_AXI

---
+++ dut.f
@@ -0,0 +1,11 @@
+
+axi_full_0.v
+AXI_FULL_M_module.v
+axi_full_v1_0.v
+axi_full_v1_0_M00_AXI.v
+axi_full_v1_0_S00_AXI.v
+design_1.v
+design_1_axi_full_0_0.v
+design_1_axi_full_1_0.v
+design_1_wrapper.v
+TOP.v
